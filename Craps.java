e116e45bc864f7c87703ff5067b11ea51ca3e70b
Jack Deschenes <jdeschenes19@students.polytechnic.org>
2019-01-18 12:54:39 -0800

    First commit

---
+++ Craps.java
@@ -0,0 +1,34 @@
+
+/**
+ * Plays Craps
+ *
+ * @author Jack Deschenes
+ * @version 2019-01-18
+ */
+public class Craps
+{
+    // instance variables - replace the example below with your own
+    private int x;
+    private boolean firstRoll;
+
+    /**
+     * Constructor for objects of class Craps
+     */
+    public Craps()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
